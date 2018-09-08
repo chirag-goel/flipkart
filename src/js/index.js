7cde9b2d1dfaf7b3ac5d6ce3d397f24fdd7a05dc
Chirag <engineerchirag021@gmail.com>
2018-09-08 12:38:00 +0530

    Assignment

---
+++ src/js/index.js
@@ -0,0 +1,94 @@
+let getJSON = function (url, callback) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+        let status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+    };
+    xhr.send();
+};
+
+getJSON('https://flipkart-mock-serve.now.sh', function (err, res) {
+    const productsIds = Object.keys(res.products.compareSummary.titles);
+    if(err) {
+        alert('Error fetching data');
+    } else{
+        createSearchBox(productsIds, res.products);
+    }
+});
+
+const createSearchBox = function (productsIds, productDetails) {
+    let selectedProducts = productsIds.slice(0, 2);
+    const searchNode = document.createElement('select');
+    const headingNode = document.getElementById('heading');
+    searchNode.innerHTML += '<option>Select to add product</option>';
+    searchNode.addEventListener('change', function (event) {
+        const selectProductId = event.target.value;
+        if (selectedProducts.indexOf(selectProductId) === -1) {
+            selectedProducts.push(event.target.value);
+            populateProductDetails(selectedProducts, productDetails);
+        } else {
+            alert("Already selected");
+        }
+
+    });
+    headingNode.appendChild(searchNode);
+    for (let productListIndex = 0; productListIndex < productsIds.length; productListIndex++) {
+        searchNode.innerHTML += `<option value="${productsIds[productListIndex]}">${productDetails.compareSummary.titles[productsIds[productListIndex]].title}</option>`;
+    }
+    populateProductDetails(selectedProducts, productDetails);
+}
+
+const populateProductDetails = function (selectedProducts, productDetails) {
+    createTable(selectedProducts, productDetails);
+    createProductHeader(productDetails.compareSummary, selectedProducts);
+}
+
+const createTable = function (productsIds, productDetails) {
+    const tableNode = document.createElement('table');
+    const productPageNode = document.getElementById('product-page');
+    productPageNode.innerHTML =+ '';
+    productPageNode.appendChild(tableNode);
+    const productFeatures = productDetails.featuresList;
+    for (let featureindex = 0; featureindex < productFeatures.length; featureindex++) {
+        const tableTrNode = document.createElement('tr');
+        tableTrNode.className = "product-feature-heading";
+        tableNode.appendChild(tableTrNode);
+        const tableThNode = document.createElement('th');
+        tableThNode.innerHTML = `${productFeatures[featureindex].title}`;
+        tableTrNode.appendChild(tableThNode);
+        const subFeatures = productFeatures[featureindex].features;
+        for (let subFeaturesIdx = 0; subFeaturesIdx < subFeatures.length; subFeaturesIdx++) {
+            const tableTrTdNode = document.createElement('tr');
+            tableNode.appendChild(tableTrTdNode);
+            const tableTdNode = document.createElement('td');
+            tableTdNode.innerHTML = `${subFeatures[subFeaturesIdx].featureName}`;
+            tableTrTdNode.appendChild(tableTdNode);
+            for (let productIndex = 0; productIndex < productsIds.length; productIndex++) {
+                const tableTdNode = document.createElement('td');
+                tableTdNode.innerHTML = `${subFeatures[subFeaturesIdx].values[productsIds[productIndex]]}`;
+                tableTrTdNode.appendChild(tableTdNode);
+            }
+        }
+    }
+}
+
+const createProductHeader = function (summary, productIds) {
+    const headerNode = document.getElementById("product-summary-wrapper");
+    headerNode.innerHTML = +'';
+    for (let productSelectedIndex = 0; productSelectedIndex < productIds.length; productSelectedIndex++) {
+        const productId = productIds[productSelectedIndex];
+        const productSummaryCode = document.createElement('div');
+        productSummaryCode.innerHTML = `<div class="product-summary"><div class="delete" onClick="deleteProduct('${productId}')">X</div><img src="${summary.images[productId]}"/> <div class="title">${summary.titles[productId].title}</div><div class="pricing"><span class="discounted-price">${summary.productPricingSummary[productId].finalPrice}</span><span class="original-price">${summary.productPricingSummary[productId].price}</span><span class="discount">${summary.productPricingSummary[productId].totalDiscount}</span></div></div>`;
+        headerNode.appendChild(productSummaryCode);
+    }
+}
+
+const deleteProduct = function(productId) {
+    console.log(`Delete - ${productId}`);
+}
